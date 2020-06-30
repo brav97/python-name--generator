7484edc8fa1c292cff7b866ba2e65b11c15a1fe6
bravian barare <bravian@localhost.localdomain>
2020-06-30 05:38:14 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+name= input('enter your name:')
+print('hello' + name)
+age= input('Please enter your age:')
+print('one more question to go')
+weight=input('please enter your weight in KG:')
+print('your name is' + name, ',your age is' + age, 'and your weight is'+ weight)
+print(' would you like us calculate your age in 10 years?')
+
+answer=input('yes or no:')
+answer_yes=True
+
+if answer_yes:
+    print('thank you')
+
+else:
+    print('Goodbye')
+
+
+
